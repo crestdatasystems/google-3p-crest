53b6adb392b86da54c4f5d47226dd08246bf2daf
Karan Panchal <karan.panchal@CLM89-50.local>
2023-05-05 18:04:25 +0530

    Updated the code to check the function

--- azure_eventhub_api_function/main.py
+++ azure_eventhub_api_function/main.py
@@ -1,59 +1,73 @@
-# Fetch the logs from Azure Event Hub and ingest them into Chronicle.
-import azure.functions as func
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Fetch the logs from Azure Event Hub and ingest them into Chronicle."""
 import json
 import logging
-import os
+from typing import List
 
-#from .common import ingest
-#from .common import utils
+import azure.functions as func
+from common import ingest
+from common import utils
 
 # Environment variable constants.
-ENV_CHRONICLE_DATA_TYPE = 'CHRONICLE_DATA_TYPE'
+ENV_CHRONICLE_DATA_TYPE = "CHRONICLE_DATA_TYPE"
 
 
-def main(events: func.EventHubEvent) -> str:
-    """Entrypoint.
+def main(events: List[func.EventHubEvent]) -> None:
+  """Entrypoint.
 
-    Args:
-        request: Request to execute the cloud function.
+  Args:
+      events: Events from the Azure Event Hub.
+  """
+  # Fetch environment variables.
+  chronicle_data_type = utils.get_env_var(ENV_CHRONICLE_DATA_TYPE)
+  logs_to_send = []
+  logging.info("Events recieved: {}".format(events))
 
-    Returns:
-        string: "Ingestion completed".
-    """
-    #chronicle_data_type = utils.get_env_var(ENV_CHRONICLE_DATA_TYPE)
-    events_to_send = []
+  logging.info("Received logs from Azure EventHub. Iterating over the logs and sending them to Chronicle.")
 
-    logging.info("Events received by the function: {}. Type: {}".format(
-        str(events), type(events)
-    ))
+  # Iterating over the list of EventHub logs to decode and JSON serialize them.
+  for event in events:
+    try:
+      records = json.loads(event.get_body().decode("utf-8"))["records"]
+    # Raise error if the event received from the Azure EventHub is not JSON
+    # serializable.
+    except json.JSONDecodeError as error:
+      print("Could not JSON serialize the Azure EventHub log.")
+      raise RuntimeError(
+          "The log data from Azure EventHub is not JSON serializable."
+      ) from error
 
-    count = 0
-    # Iterating over the list of event hub logs to decode and json serialize them.
-    for event in events:
-        try:
-            logging.info("Record received: {}".format(
-                event.get_body().decode('utf-8')
-            ))
-            records = json.loads(event.get_body().decode('utf-8'))['records']
-            logging.info("Parsing record: {}".format(records))
-        # Raise error if the event received from the event hub is not json serializeable. 
-        except json.JSONDecodeError as error:
-            print("Could not json serialize the event hub log")
-            raise RuntimeError(f"The log data from event hub is not JSON serializable") from error
-        events_to_send.append(records)
-        count = count + 1
-        if count == 3:
-            logging.info("Breaking the loop now to end the testing..")
-            break
+    # Events are nested in the list form in the EventHub log message.
+    if isinstance(records, list):
+      logs_to_send.extend(records)
+    else:
+      logs_to_send.append(records)
 
-    logging.info("Events planned to send to Azure: {}".format(
-        str(events_to_send)
-    ))
+  logs_count = len(logs_to_send)
+  logging.info("Parsed {} events from Azure EventHub. Sending them to Chronicle.".format(
+    logs_count
+  ))
 
-    # try:
-    #     # Ingest Event hub logs to chronicle.
-    #     ingest.ingest(events_to_send, chronicle_data_type)
-    # except Exception as error:
-    #     raise Exception("Unable to push the data to the Chronicle.") from error
+  try:
+    # Ingest Azure EventHub logs to Chronicle.
+    ingest.ingest(logs_to_send, chronicle_data_type)
+  except Exception as error:
+    raise Exception("Unable to push the data to the Chronicle.") from error
 
-    logging.info(f"Total {len(events_to_send)} log(s) are successfully ingested to Chronicle.")
+  logging.info(
+      "Total %s log(s) are successfully ingested to Chronicle.",
+      logs_count,
+  )
